27bb1d6c359116fa3617ef87dfe4d47c55c425d2
Roman <exculibur@rambler.ru>
2017-10-09 13:39:08 +0300

    Update test_002.cpp

--- test_002.cpp
+++ test_002.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 
+using namespace std;
+
 template <typename Iterator, typename T>
-auto find_two_elements_with_sum(Iterator first, Iterator last, T c) -> std::pair<bool, std::pair<Iterator, Iterator>> {
-	for (; first != last; ++first) {
-		if ((first + (++first)) == c) {
-			return (true, (first, ++first));
+auto find_two_elements_with_sum(Iterator first, Iterator last, T c) {
+	bool t_i = false;
+	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+
+	if (first == last || first > last) cout << "ERROR: range." << endl;
+	if (c < 1 || c > 13) cout << "ERROR: value." << endl;
+
+	while (first != last) {
+		int summ = arr[first] + arr[last];
+		if (summ < c)
+			first++;
+		else if (summ > c)
+			last--;
+		else {
+			t_i = true;
+			cout << "Our Iterators " << first << " " << last << endl;
+			break;
 		}
-		--first;
 	}
-};
+	return 0;
+}
 
 int main() {
+	int s_e = 14;
+	size_t a = 0, b = 9;
+	find_two_elements_with_sum(a, b, s_e);
+
 	return 0;
 }
